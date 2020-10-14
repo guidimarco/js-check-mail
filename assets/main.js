4ff29cd0e4796b6ff89b2fd426ecc749e210c6a8
Marco Guidi <mark.88g@gmail.com>
2020-10-14 19:15:54 +0200

    cambiato algoritmo e stilata la pagina

--- assets/main.js
+++ assets/main.js
@@ -1,22 +1,25 @@
 // DICHIARAZIONE VARIABILI
 var mailList = ["mario.rossi@gmail.com", "bianchi66mario@outlook.com", "marco_verdi@hotmail.it", "elisaesposito_1991@alice.it"];
 
-// PROVA CICLO MAIL LIST
-// for (var i = 0; i < mailList.length; i++) {
-//     console.log(mailList[i]);
-// }
-
-var logInMail = prompt("Inserisci la tua mail")
-
+var logInMail = prompt("Inserisci la tua mail");
 
 console.log(mailList);
 console.log(logInMail);
 
 // VERIFICA PRESENZA NELLA MAIL LIST
+// Variabile di controllo: di default 0 --> diventa 1 se la mail è nella lista
+var control = 0;
+
 for (var i = 0; i < mailList.length; i++) {
     if (mailList[i] == logInMail) {
-        console.log("OK");
-    } else {
-        console.log("NO");
+        control = control + 1;
     }
+}
+
+console.log(control);
+
+if (control != 0) {
+    document.getElementById("log-in-message").innerHTML = "Great! You're in!";
+} else {
+    document.getElementById("log-in-message").innerHTML = "You shall not pass!";
 }
